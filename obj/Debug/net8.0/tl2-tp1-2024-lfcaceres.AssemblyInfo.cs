94907345d90397dfef9a88651edee2c9e412ae1a
luis fernando caceres <luisfernandocaceres@MacBook-Air-de-luis.local>
2024-08-27 08:52:36 -0300

    hola

--- obj/Debug/net8.0/tl2-tp1-2024-lfcaceres.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp1-2024-lfcaceres.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp1-2024-lfcaceres")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a520daa0a3f75648ff46bc42df21b2d1975d8fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp1-2024-lfcaceres")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp1-2024-lfcaceres")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
